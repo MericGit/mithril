44eb13b09aabb823f1f8e606fd5c37f05956861f
MericGit <41242144+MericGit@users.noreply.github.com>
2025-03-29 13:48:47 -0400

    Update agiClient.py

--- backend/src/backend/clients/agiClient.py
+++ backend/src/backend/clients/agiClient.py
@@ -20,18 +20,88 @@ class AGIClient:
     def generate_summary(self, file):
         LOCAL_DATA = Path(__file__).resolve().parent.parent / "local_data"
         print("LOCAL DATA" , LOCAL_DATA)
-        SYS_PROMPT = """Given this research paper, summarize it into its core parts. You do not need to summarize its technical details rather summarize what field the paper is in, its novel applications, and any other key components"""
-        file = self.client.files.upload(file=LOCAL_DATA / file)
+        SYS_PROMPT = """Given this research paper, summarize it into its core parts. You do not need to summarize its technical details rather summarize what field the paper is in, its novel applications, and any other key components. Additionally give me the list of authors, and also list which of the following topics below that tha paper references (can be multiple!)
+        topics = [
+AI,
+Semiconductors,
+Life Sciences,
+Nuclear,
+Energy,
+Quantum Computing,
+Biotechnology,
+Robotics,
+Nanotechnology,
+Cybersecurity,
+Space Exploration,
+Environmental Science,
+Advanced Materials,
+Telecommunications,
+Defense Technology,
+Data Science,
+High-Performance Computing,
+Synthetic Biology,
+Climate Technology,
+Autonomous Systems,
+5G/6G Networks,
+Geopolitical Tech Strategy,
+Innovation Policy,
+Digital Infrastructure
+]"""
+        #file = self.client.files.upload(file=LOCAL_DATA / file)
         full_prompt = f"""{SYS_PROMPT}"""
-        response = self.client.models.generate_content(
-            model=self.model,
-            contents=[
-                file,
-                "\n\n",
-                full_prompt
-            ],
+        files = [
+            # Please ensure that the file is available in local system working direrctory or change the file path.
+            self.client.files.upload(file=LOCAL_DATA / file),
+        ]
+        contents = [
+            types.Content(
+                role="user",
+                parts=[
+                    types.Part.from_uri(
+                        file_uri=files[0].uri,
+                        mime_type=files[0].mime_type,
+                    ),
+                    types.Part.from_text(text=full_prompt),
+                ],  
+            ),
+        ]
+        generate_content_config = types.GenerateContentConfig(
+            response_mime_type="application/json",
+            response_schema=genai.types.Schema(
+                type = genai.types.Type.OBJECT,
+                required = ["authors", "topics", "risk_analysis", "topics_relevence"],
+                properties = {
+                    "authors": genai.types.Schema(
+                        type = genai.types.Type.ARRAY,
+                        items = genai.types.Schema(
+                            type = genai.types.Type.STRING,
+                        ),
+                    ),
+                    "topics": genai.types.Schema(
+                        type = genai.types.Type.ARRAY,
+                        items = genai.types.Schema(
+                            type = genai.types.Type.STRING,
+                        ),
+                    ),
+                    "risk_analysis": genai.types.Schema(
+                        type = genai.types.Type.STRING,
+                    ),
+                    "topics_relevence": genai.types.Schema(
+                        type = genai.types.Type.ARRAY,
+                        items = genai.types.Schema(
+                            type = genai.types.Type.INTEGER,
+                        ),
+                    ),
+                },
+            ),
         )
-        print(response.text)
+
+        for chunk in self.client.models.generate_content_stream(
+            model=self.model,
+            contents=contents,
+            config=generate_content_config,
+        ):
+            print(chunk.text, end="")
     
 client = AGIClient()
 client.generate_summary("Lecun98.pdf")
